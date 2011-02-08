3a8f7bbb7dd1b499f285eff485f93eacbbeafa9d
Brandon Liu <brandon.k.liu@gmail.com>
2011-02-07 21:31:11 -0500

    Local config files are no longer in the repo

--- application/logs/2011/02/02.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2011-02-02 01:14:15 --- ERROR: ReflectionException [ -1 ]: Class controller_tthv does not exist ~ SYSPATH\classes\kohana\request.php [ 1178 ]
-2011-02-02 01:14:36 --- ERROR: ReflectionException [ -1 ]: Class controller_tthv does not exist ~ SYSPATH\classes\kohana\request.php [ 1178 ]
-2011-02-02 01:28:56 --- ERROR: ErrorException [ 4 ]: syntax error, unexpected '[' ~ APPPATH\classes\model\case.php [ 6 ]
