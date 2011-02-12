fa643452983fffbf508c11bce9a7241ac22496be
Brandon Liu <brandon.k.liu@gmail.com>
2011-02-12 11:57:27 -0500

    Added cron job, added boilerplate sms code

--- application/classes/controller/cron.php
+++ application/classes/controller/cron.php
@@ -4,10 +4,72 @@ class Controller_Cron extends Controller {
   public function action_index() {
     $model = Model::factory('appointment');
     $appts = $model->select_all();
-    $now = time();
-    for($appts as $appt) {
-      sdf
-    }
-	}
 
-} // End Welcome
+    $start = new DateTime();
+    $start->add(DateInterval::createFromDateString('7 days'));
+    $end = $start->add(DateInterval::createFromDateString('1 days'));
+    $start = $start->getTimestamp();
+    $end = $end->getTimestamp();
+
+    foreach($appts as $appt) {
+      if ($appt['date'] > $start AND $appt['date'] < $end) {
+        $this->sendReminder($appt);
+      }
+      else if ($appt['date'] > $end) {
+        break;
+      }
+    }
+  }
+
+  private function sendReminder($appt) {
+    $now = new DateTime();
+    $fp = fopen("/home/brandonkliu/public_html/tthv/application/logs/reminders.txt", "w+");
+    $output = "Message: \"{$appt['message']}\" sent {$now->format("M j, Y")}";
+    fwrite($fp, $output);
+    echo $output;
+
+    //$this->sendSMS($appt);
+  }
+
+  private function sendSMS($appt) {
+    $mobile = Model::factory('case')->select_by_id($appt['case_id']);
+
+    //copied from smscountry.com
+    $user="XXXXXX"; //your username
+    $password="XXXXXX"; //your password
+    $mobilenumbers=$mobile; //enter Mobile numbers comma seperated
+    $message = $appt['message']; //enter Your Message 
+    $senderid="SMSCountry"; //Your senderid
+    $messagetype="N"; //Type Of Your Message
+    $DReports="Y"; //Delivery Reports
+    $url="http://www.smscountry.com/SMSCwebservice.asp";
+    $message = urlencode($message);
+    $ch = curl_init(); 
+    if (!$ch){die("Couldn't initialize a cURL handle");}
+    $ret = curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt ($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);          
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt ($ch, CURLOPT_POSTFIELDS, 
+      "User=$user&passwd=$password&mobilenumber=$mobilenumbers&message=$message&sid=$senderid&mtype=$messagetype&DR=$DReports");
+    $ret = curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $curlresponse = curl_exec($ch); // execute
+    if(curl_errno($ch))
+      echo 'curl error : '. curl_error($ch);
+
+    if (empty($ret)) {
+      // some kind of an error happened
+      die(curl_error($ch));
+      curl_close($ch); // close cURL handler
+    } 
+    else {
+      $info = curl_getinfo($ch);
+      curl_close($ch); // close cURL handler
+        //echo "<br>";
+        echo $curlresponse;    //echo "Message Sent Succesfully" ;
+
+    }
+  }
+
+}
