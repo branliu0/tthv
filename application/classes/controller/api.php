7edb9d76ba77f0d8d0ea4fb5b99d5b263db89563
Brandon Liu <brandon.k.liu@gmail.com>
2011-02-28 12:55:42 -0500

    completed API for addPatient, addAppointment, and getAppointments

--- application/classes/controller/api.php
+++ application/classes/controller/api.php
@@ -2,8 +2,61 @@
 
 class Controller_Api extends Controller {
 	public function action_index() {
-		$data['foo'] = 'bar';
-		echo json_encode($data);
+    if (!isset($_POST['action'])) {
+      echo json_encode(array("success" => false, "errors" => array("Please provide an API action")));
+    }
+    switch ($_POST['action']) {
+    case "addPatient":
+      $post = Validate::factory($_POST);
+      $post->filter(TRUE, 'trim');
+      $post->rule('patient_name', 'not_empty');
+      $post->rule('village_name', 'not_empty');
+      $post->rule('phc_name', 'not_empty');
+      $post->rule('phc_name', 'regex', array('/^[a-zA-Z0-9_\- ]+$/'));
+      $post->rule('mobile', 'not_empty');
+      $post->rule('mobile', 'numeric');
+      $post->rule('mobile', 'exact_length', array(10));
+
+      if ($post->check()) {
+        Model::factory('case')->add($post->as_array());
+        echo json_encode(array("success" => true));
+      }
+      else {
+        $errors = $post->errors('validate');
+        echo json_encode(array("success" => false, "errors" => $errors));
+      }
+      break;
+    case "addAppointment":
+      $post = Validate::factory($_POST);
+      $post->filter(TRUE, 'trim');
+      $post->rule('child_name', 'not_empty');
+      $post->rule('birth_date', 'not_empty');
+      $post->rule('birth_date', 'date');
+
+      if (!isset($_POST['case_id'])) {
+        echo json_encode(array("success" => false, "errors" => array("Please include the case id of the patient")));
+        break;
+      }
+      if ($post->check()) {
+        $appts->add_child($post);
+        echo json_encode(array("success" => true));
+      }
+      else {
+        $errors = $post->errors('validate');
+        echo json_encode(array("success" => false, "errors" => $errors));
+      }
+      break;
+    case "getAppointments":
+      if (!isset($_POST['id'])) {
+        echo json_encode(array("success" => false, "errors" => array("Please include the case id of the patient")));
+        break;
+      }
+      $appointments = Model::factory('appointment')->select_by_case_id($_POST['id']);
+      echo json_encode(array("success" => true, "id" => $_POST['id'], "appointments" => $appointments));
+      break;
+    default:
+      echo json_encode(array("success" => false, "errors" => array("Please use a valid API action")));
+    }
 	}
 
 } // End Welcome
